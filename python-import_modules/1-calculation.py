10964d7bb98481552bc4df1c5ac0900539ad74c9
iranziprince01 <p.iranzi@alustudent.com>
2023-02-27 09:40:56 -0800

    Task 1 update

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#python program to display calculating operations
+# python program to display calculating operations
 
 if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     a = 10
     b = 5
 
-    #display the results
+# display the results
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
