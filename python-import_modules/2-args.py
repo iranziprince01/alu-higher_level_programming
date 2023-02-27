c6fea07a22138126cbc66f7a852167af4ad7edb9
iranziprince01 <p.iranzi@alustudent.com>
2023-02-27 10:09:52 -0800

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,8 +8,10 @@ if __name__ == '__main__':
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+        if count == 1:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+
     else:
         print("{} arguments:".format(count))
         for i in range(count):
