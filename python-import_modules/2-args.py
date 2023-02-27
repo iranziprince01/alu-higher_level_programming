300f66b29f140e358f36f30f04f9b5418e0443cc
iranziprince01 <p.iranzi@alustudent.com>
2023-02-27 10:05:12 -0800

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 # python program to display arguments of the word
 
 if __name__ == '__main__':
@@ -6,10 +7,11 @@ if __name__ == '__main__':
 
     count = len(sys.argv) - 1
     if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("() arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i +1, sys.argv[i + 1]))
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+        for i in range(count):
+            print("{}: {}".format(i +1, sys.argv[i + 1]))
+
