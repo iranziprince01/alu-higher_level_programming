92d3168356f8735eb4fafdea11ea3cb8f670f80c
iranziprince01 <p.iranzi@alustudent.com>
2023-02-27 04:52:28 -0800

    Task 1

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# import the add function from add_0.py
+
+def add(a, b):
+
+# define variables a and b
+a = 1
+b = 2
+
+# call the add function with arguments a and b and store the result in a variable called result
+result = add(a, b)
+
+# print the result using string formatting
+print("{} + {} = {}".format(a, b, result))
+
