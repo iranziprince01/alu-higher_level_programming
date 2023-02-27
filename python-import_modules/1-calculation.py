482ea81a9f1f166ed1c5380aab1106556c3f3e0e
iranziprince01 <p.iranzi@alustudent.com>
2023-02-27 05:49:06 -0800

    Task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,19 +4,18 @@
 from calculator_1 import add, subtract, multiply, divide
 
 if __name__ == '__main__':
-
-# define variables a and b
+    # define variables a and b
 a = 10
 b = 5
 
-# perform some math operations
-sum_ab = add(a, b)
+# perform the calculations
+sum_ab = a + b
 diff_ab = subtract(a, b)
-prod_ab = multiply(a, b)
-quot_ab = divide(a, b)
+prod_ab = a * b
+quot_ab = a / b
 
-# display the results
-print("The sum of {} and {} is: {}".format(a, b, sum_ab))
-print("The difference between {} and {} is: {}".format(a, b,diff_ab))
-print("The Product of {} and {} is: {}".format(a, b, prod_ab))
-print("The qotient of {} and {} is: {}".format(a, b, quot_ab))
+# print the results
+print("{} + {} = {}".format(a, b, sum_ab))
+print("{} - {} = {}".format(a, b, diff_ab))
+print("{} * {} = {}".format(a, b, prod_ab))
+print("{} / {} = {}".format(a, b, quot_ab))
