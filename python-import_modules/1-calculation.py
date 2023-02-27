53e4424fe1263fc88feffd803fd6e5cf3bbf0e60
iranziprince01 <p.iranzi@alustudent.com>
2023-02-27 05:24:36 -0800

    Task 1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# import functions from calculation file
+from calculator_1 import add, subtract, multiply, divide
+
+# define variables a and b
+a = 10
+b = 5
+
+# perform some math operations
+sum_ab = add(a, b)
+diff_ab = subtract(a, b)
+prod_ab = multiply(a, b)
+quot_ab = divide(a, b)
+
+# print the results
+print(f"The sum of {a} and {b} is {sum_ab}.")
+print(f"The difference between {a} and {b} is {diff_ab}.")
+print(f"The product of {a} and {b} is {prod_ab}.")
+print(f"The quotient of {a} and {b} is {quot_ab}.")
