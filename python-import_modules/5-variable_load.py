454c2109e6414a10e97663a31e9644b15283037d
iranziprince01 <p.iranzi@alustudent.com>
2023-02-27 13:01:30 -0800

    Task 5

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# program that imports the variable a from the file
+
+from variable_load_5 import a
+
+def find_var():
+    print(a)
+
+if __name == "__main__":
+    find_var()
+
