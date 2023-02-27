bb56aae226ae5722178f6e0ebc08c9c790b73d42
iranziprince01 <p.iranzi@alustudent.com>
2023-02-27 10:38:27 -0800

    Task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# python program to display sum of arguments
+
+if __name__ == '__main__':
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys,argv[i + 1])
+        print("{}".format(total))
