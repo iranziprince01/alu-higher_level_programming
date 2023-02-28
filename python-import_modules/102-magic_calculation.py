134ccce5454eae255e6510aa47e428246d8e685f
iranziprince01 <p.iranzi@alustudent.com>
2023-02-28 03:47:53 -0800

    Task 8

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -4,9 +4,9 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
+        m = add(a, b)
+        for j in range(4, 6):
+            m = add(m, j)
+        return (m)
     else:
         return (sub(a, b))
