5275a595d7517419177477f570f3647f62c78967
iranziprince01 <p.iranzi@alustudent.com>
2023-03-07 05:35:28 -0800

    Task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# a function that returns a tuple with the length of a 
-#+ string and its first character.
+# a function that returns a tuple with the length of a
+# +string and its first character.
 
 
 def multiple_returns(sentence):
