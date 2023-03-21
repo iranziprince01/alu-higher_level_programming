c549f1bc04542ebca540b57156e1e5db685a3881
iranziprince01 <p.iranzi@alustudent.com>
2023-03-21 04:38:14 -0700

    Task update

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -4,23 +4,40 @@
 
 import sys
 
-total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+metrics = {"total_size": 0, "status_code_count": {code: 0 for code in status_codes}}
 
 try:
     for i, line in enumerate(sys.stdin):
-        ip_address, _, _, _, _, status_code, file_size = line.split()
-        total_file_size += int(file_size)
-        status_code_counts[int(status_code)] += 1
-
-        if (i + 1) % 10 == 0:
-            print("Total file size: File size: {}".format(total_file_size))
-            for status_code, count in sorted(status_code_counts.items()):
-                if count > 0:
-                    print("{}: {}".format(status_code, count))
-
+        if i % 10 == 0 and i > 0:
+            print("Total file size: File size: {}".format(metrics["total_size"]))
+            for code in sorted(metrics["status_code_count"].keys()):
+                if metrics["status_code_count"][code] > 0:
+                    print("{}: {}".format(code, metrics["status_code_count"][code]))
+        parts = line.split()
+        code = parts[3]
+        size = int(parts[4])
+        metrics["total_size"] += size
+        if code in metrics["status_code_count"]:
+            metrics["status_code_count"][code] += 1
 except KeyboardInterrupt:
-    print("Total file size: File size: {}".format(total_file_size))
-    for status_code, count in sorted(status_code_counts.items()):
-        if count > 0:
-            print("{}: {}".format(status_code, count)))
+    print("Total file size: File size: {}".format(metrics["total_size"]))
+    for code in sorted(metrics["status_code_count"].keys()):
+        if metrics["status_code_count"][code] > 0:
+            print("{}: {}".format(code, metrics["status_code_count"][code]))
+
+
+"""
+This module reads input from stdin in the following format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+
+For every 10 lines of input or when a keyboard interruption (CTRL + C) is detected, the module prints the following statistics since the beginning:
+- Total file size: File size: <total size> (sum of all previous file sizes)
+- Number of lines by status code (in ascending order):
+    <status code>: <number>
+
+Possible status codes: 200, 301, 400, 401, 403, 404, 405, and 500.
+
+Note: This module assumes that the input format is consistent with the format specified in the prompt and does not handle any errors that may arise from malformed input.
+"""
+
