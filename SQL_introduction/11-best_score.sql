8774c55e0413f43bce0d187d12dba8de56fca173
iranziprince01 <p.iranzi@alustudent.com>
2023-03-28 12:09:18 -0700

    a script that lists all records with a score >= 10 in the table second_table

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- List all records of the `second_table` by `score` in descending orderscore>=10
-SELECT score, name FROM `second_table` ORDER BY score desc WHERE score >= 10;
+SELECT score, name FROM `second_table` WHERE `score` >= 10 ORDER BY `score` desc;
