8f131a6d9781e34f36db6ac86bfe2fee66b47b1b
iranziprince01 <p.iranzi@alustudent.com>
2023-03-30 10:31:12 -0700

    ists all genres in the database hbtn_0d_tvshows_rate

---
+++ SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- SQL script to  list all genres in the database
+SELECT g.name, SUM(r.rate) AS rating
+FROM tv_genres g INNER JOIN tv_show_genres sg
+  ON g.id = sg.genre_id
+  INNER JOIN tv_show_ratings r
+  ON sg.show_id = r.show_id
+GROUP BY g.name
+ORDER BY SUM(r.rate) desc;
