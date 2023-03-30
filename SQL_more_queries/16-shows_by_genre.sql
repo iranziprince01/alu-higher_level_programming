df8f03d7e80281c3a534713d1c27f057b07b9228
iranziprince01 <p.iranzi@alustudent.com>
2023-03-30 10:10:39 -0700

    Linked genres to shows

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- SQL script that lists all shows, and all genres linked to that show
+SELECT s.title, g.name
+FROM tv_shows s LEFT OUTER JOIN tv_show_genres sg
+  ON s.id = sg.show_id
+  LEFT OUTER JOIN tv_genres g
+  ON sg.genre_id = g.id
+ORDER BY s.title, g.name;
