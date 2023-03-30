923dca9b94caab23bd5e6adbd431377a4888d2d6
iranziprince01 <p.iranzi@alustudent.com>
2023-03-30 03:25:50 -0700

    count number of genres in each show

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Sql querry to list number of genres inside shows
+SELECT g.name AS genre, COUNT(gs.genre_id) AS number_of_shows
+FROM tv_shows s INNER JOIN tv_show_genres gs
+  ON s.id = gs.show_id
+  INNER JOIN tv_genres g
+  ON gs.genre_id = g.id
+GROUP BY g.name
+ORDER BY COUNT(gs.genre_id) desc;
