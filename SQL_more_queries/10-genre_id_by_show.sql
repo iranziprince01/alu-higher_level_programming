e487fe6e824905f961860c1a1e53a6f58ae1a738
iranziprince01 <p.iranzi@alustudent.com>
2023-03-30 03:13:09 -0700

    lists all tv_shows content genre.

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT s.title AS title, g.genre_id AS genre_id
+FROM tv_shows s INNER JOIN tv_show_genres g
+  ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
