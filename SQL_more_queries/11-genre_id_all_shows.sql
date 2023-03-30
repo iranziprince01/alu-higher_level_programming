bf75a07c04eea01510af21918875c78d74214778
iranziprince01 <p.iranzi@alustudent.com>
2023-03-30 03:18:23 -0700

    lists all shows contained in the database hbtn_0d_tvshows

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- a script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT s.title AS title, g.genre_id AS genre_id
+FROM tv_shows s LEFT OUTER JOIN tv_show_genres g
+  ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
