62619a8fb73f8da2d75ed25f59e4980a3870b99b
iranziprince01 <p.iranzi@alustudent.com>
2023-06-13 08:54:20 -0700

    argument checking

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv[3])
-{
+if (process.argv[3]) {
 	console.log('Arguments found');
-} else if (process.argv[2])
-{
-	console.log('Arguments found');
-} else
-{
+} else if (process.argv[2]) {
+	console.log('Argument found');
+} else {
 	console.log('No argument');
 }
